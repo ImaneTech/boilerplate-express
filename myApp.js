878c53398c311d0c0b8ceedc09c2d5e63844be70
ImaneTech <imane.zouine100@gmail.com>
2025-04-19 17:18:42 +0100

    start a working Express Server + serve an HTML File / Static Assets / JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -1,5 +1,27 @@
 let express = require('express');
 let app = express();
+//1
+// console.log("Hello World");
+
+//2
+//app.get("/",(req,res) => {
+//res.send("Hello Express")
+//})
+
+//4
+app.use('/public', express.static(__dirname + '/public'));
+
+//3
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+  })
+
+  //5 
+  app.get("/json",(req,res)=>{
+    res.json({"message":"Hello json"})
+  }
+
+)
 
 
 
@@ -10,30 +32,4 @@ let app = express();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
